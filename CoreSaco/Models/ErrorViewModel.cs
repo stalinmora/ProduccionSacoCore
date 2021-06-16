55660da06ccb3b063fb640c1d2434572e4e02c99
stalinmora <stalin.mora.burgos@gmail.com>
2021-06-16 17:55:00 -0500

    Primer commit

---
+++ CoreSaco/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreSaco.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
